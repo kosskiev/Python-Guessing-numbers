443cc0f411a648cae5818e9f28b643cbb98c83bb
kosskiev <35668277+kosskiev@users.noreply.github.com>
2020-11-03 16:25:26 +0200

    Update Guessing-numbers.py

--- Guessing-numbers.py
+++ Guessing-numbers.py
@@ -1,15 +1,13 @@
 import random
-
-counter = 0
-
+counter = 0 # подсчитывать количество попыток
 print('Привет как тебя зовут ')
 name = input()
 
 for i in range(1):
-    num = random.randint(1, 100)
+    num = random.randint(1, 100) # получить случайное число от 0 до 100
 
 def guessing_numbers():
-    while num > 0:
+    while num > 0: # пока случайное число больше 0
         a = int(input(name +', введите число от 1 до 100: \n' ))
         if a == num:
             print('Вы угадали, поздравляем!')
@@ -20,7 +18,7 @@ def guessing_numbers():
             print('Слишком мало, попробуйте еще раз')
             continue
 
-#print(num)
+#print(num) # можно отобразить случайное число для удобства проверки
 print(guessing_numbers())
 
 
