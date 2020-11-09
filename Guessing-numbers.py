f0bcf47c51fe3389ef551456e83c68536c2d37cf
kosskiev <35668277+kosskiev@users.noreply.github.com>
2020-11-09 13:10:34 +0200

    Update Guessing-numbers.py

--- Guessing-numbers.py
+++ Guessing-numbers.py
@@ -9,23 +9,30 @@ def is_valid(n):
     return False
 counter = 1  # подсчитывать количество попыток
 random_number = random.randint(1, 100) # получить случайное число от 0 до 100
-while True:
-    answer = int(input(name +', введите число от 1 до 100: \n' ))
-    if is_valid(answer) == False:
-        print('Введите число согласно условию')
-        counter += 1
-        continue
-    if answer == random_number:
-        print('Вы угадали, поздравляем!')
-        print(f'Количество ваших попыток {counter}')
-        break
-    if 101 > answer > random_number:
-        print('Слишком много, попробуйте еще раз')
-        counter += 1
-    if 0< answer < random_number:
-        print('Слишком мало, попробуйте еще раз')
-        counter +=1
-    continue
+repeat = 'д'
+while repeat.lower() == 'д':
+    while True:
+        print(name +', введите число от 1 до 100: ' )
+        answer = int(input())
+        if is_valid(answer) == False:
+            print('Введите число согласно условию')
+            counter += 1
+            continue
+        if answer == random_number:
+            print('Вы угадали, поздравляем!')
 
+            break
+        if 101 > answer > random_number:
+            print('Слишком много, попробуйте еще раз')
+            counter += 1
+            continue
+        if 0< answer < random_number:
+            print('Слишком мало, попробуйте еще раз')
+            counter +=1
+            continue
+    print(f'Количество ваших попыток: {counter}!')
+    print("Хотите сыграть снова? д = да, н = нет")
+    repeat = input()
+print("Спасибо, что играли в числовую угадайку. Еще увидимся... :)")
 
 
